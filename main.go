17d9147303db0e0b1479e58430cee598ec360f95
Mairbek Khadikov <mairbek@conduit.xyz>
2025-07-29 11:01:35 -0700

    remove useless interceptor

--- main.go
+++ main.go
@@ -6,8 +6,6 @@ import (
 	"sona/service"
 
 	"sona/gen/sonav1connect"
-
-	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
@@ -19,28 +17,8 @@ func main() {
 	helloPath, helloHandler := sonav1connect.NewHelloServiceHandler(helloServer)
 	mux.Handle(helloPath, helloHandler)
 
-	// Legacy router
-	router := httprouter.New()
-	router.HandlerFunc(http.MethodPost, "/api/v1/pepe", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("Pepuuu"))
-	})
-
-	mux.Handle("/api/", router)
-
-	// Create and mount the path rewrite interceptor
-	// Define path rewrite rules
-	pathRewriteRules := []service.PathRewriteRule{
-		{
-			From: "/api/v1/hello",
-			To:   "/sona.v1.HelloService/Hello",
-		},
-		// Add more rules here as needed
-	}
-	interceptor := service.NewPathRewriteInterceptor(pathRewriteRules, mux)
-
 	log.Println("Server starting on :8080...")
-	if err := http.ListenAndServe(":8080", interceptor); err != nil {
+	if err := http.ListenAndServe(":8080", mux); err != nil {
 		log.Fatal(err)
 	}
 }
